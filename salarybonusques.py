02419f52fd15035fd627278417651a146acdd645
heyyprince09 <114799883+heyyprince09@users.noreply.github.com>
2022-10-08 15:23:48 +0530

    Add files via upload
    Maths Ques.

---
+++ salarybonusques.py
@@ -0,0 +1,11 @@
+Ques.-a company decided to give bonus of 1000Rs to employee if his/her service is more than 5 years ask user thier salary 
+      and year service and print the net bonus amount.
+
+salary = int(input("enter 1st number"))
+year = int(input("enter 2nd number"))
+bonus = salary + 1000
+if year > 5 :
+    print(bonus)
+
+else :
+    print("sorry")
