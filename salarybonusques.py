bc9cb24dca37b4a26031469cb2993bb717a92e98
heyyprince09 <114799883+heyyprince09@users.noreply.github.com>
2022-12-11 12:10:58 +0530

    commit

--- salarybonusques.py
+++ salarybonusques.py
@@ -1,5 +1,4 @@
-Ques.-a company decided to give bonus of 1000Rs to employee if his/her service is more than 5 years ask user thier salary 
-      and year service and print the net bonus amount.
+
 
 salary = int(input("enter 1st number"))
 year = int(input("enter 2nd number"))
